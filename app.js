8202714cb89f3e0776c3c97e0f85d5ec7a65efd1
sashasohrabi <shb.sohrabi@gmail.com>
2018-10-06 23:27:41 +0300

    class UI added.

--- app.js
+++ app.js
@@ -6,3 +6,59 @@ class Book {
     }
 }
 
+class UI {
+    addBookToList(book) {
+        const list = document.getElementById
+        ('book-list');
+        // Create tr element
+        const row = document.createElement('tr');
+        // Insert cols
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="delete">x</a>
+            </td>
+        `;
+
+        list.appendChild(row);
+    }
+
+    showAlert(message, className) {
+
+        // Create div
+        const div = document.createElement('div');
+        // Add classes
+        div.className = `alert ${className}`;
+        // Add text
+        div.appendChild(document.createTextNode
+        (message));
+        // Get parent
+        const container = document.querySelector
+        ('.container');
+        // Get form
+        const form = document.querySelector
+        ('#book-form');
+        // Insert alert
+        container.insertBefore(div, form);
+
+        // Timeout after 3 sec
+        setTimeout(function () {
+            document.querySelector('.alert').remove();
+        }, 3000);
+
+    }
+
+    deleteBook(target) {
+        if(target.className === 'delete') {
+            target.parentElement.parentElement.remove();
+        }
+    }
+
+    clearFields() {
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('isbn').value = '';
+
+    }
+}
